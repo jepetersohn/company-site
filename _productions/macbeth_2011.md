3bb5eced5e7cfadb50b4b6e679042d0c1b3d1425
Mhar Andal <mharandal96@gmail.com>
2017-03-19 07:45:58 -0500

    Fix layout

--- _productions/macbeth_2011.md
+++ _productions/macbeth_2011.md
@@ -31,12 +31,8 @@ cast:
     actor_bio_url: http://accidentalshakespeare.com/about/company/julia_kessler
 
 images:
-  - url: /assets/images/Macbeth_flier.pdf
   - url: /assets/images/Macbethreading_3witches.jpg
   - url: /assets/images/Macbethreading_Oakes_Jaime.jpg
   - url: /assets/images/macbethreading_cast.jpg
   - url: /assets/images/Macbethreading_Shatkus.jpg
 ---
-
-
-
---
+++ assets/js/carousel-swipe.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+          //add your other targets here
+       $(".carousel").swiperight(function() {
+          $(this).carousel('prev');
+          });
+//add your other targets here
+       $(".carousel").swipeleft(function() {
+          $(this).carousel('next');
+     });
+
+      jQuery(document).ready(function($) {
+
+      $('#myCarousel').carousel({
+              interval: 5000
+      });
+
+      $('#carousel-text').html($('#slide-content-0').html());
+
+      //Handles the carousel thumbnails
+     $('[id^=carousel-selector-]').click( function(){
+          var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+          var id = parseInt(id);
+          $('#myCarousel').carousel(id);
+      });
+
+
+      // When the carousel slides, auto update the text
+      $('#myCarousel').on('slid.bs.carousel', function (e) {
+               var id = $('.item.active').data('slide-number');
+              $('#carousel-text').html($('#slide-content-'+id).html());
+      });
+});
+  });
